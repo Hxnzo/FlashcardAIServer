bfbc4a71735888333f858f6d2aae10c5dcfd1cc3
Hanzalah Patel <hanzalahp20@gmail.com>
2025-03-21 17:19:35 -0400

    test

--- server/server.js
+++ server/server.js
@@ -186,4 +186,4 @@ app.post('/generate-flashcards', async (req, res) => {
 });
 
 // Start the server on the specified port
-app.listen(port, () => console.log(`Server running on http://localhost:${port}`));
+app.listen(port, () => console.log(`Server running on http://localhost:${port}`)); 
